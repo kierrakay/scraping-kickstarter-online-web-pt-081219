869b281840af5212c5e3d58ff2d4d9d5ac4b13d4
Kierra M <k.merriman32@hotmail.com>
2019-10-04 22:01:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,8 +17,19 @@ require "pry"
 #   # return the projects hash
 #   projects
 # end
+     
+      #refactor of above is below so it doesnt give huge nokogir obect
+ def create_project_hash
+  projects = {}
  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
  
+  # return the projects hash
+  projects
+end
  
  
  
