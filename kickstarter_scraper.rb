e8a18c4a071c43ab0c73dfe00248ffba9d0dc78f
Kierra M <k.merriman32@hotmail.com>
2019-10-04 22:04:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -27,7 +27,9 @@ require "pry"
  
     kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      
+    }
   end
  #using the to_sym method. Remember that symbols make better hash keys than strings.
  
