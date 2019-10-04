77912c70124240caf474e32f900d0027ac7329ad
Kierra M <k.merriman32@hotmail.com>
2019-10-04 22:05:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -45,6 +45,7 @@ end
  
 create_project_hash
 # type project(whatever the class for html is and go in terminal with this after finding what you need to save it ... project_location=_ will save the last info in this case location into a variable)
+
  # projects: kickstarter.css("li.project.grid_4")
  
  # title: project.css("h2.bbcard_name strong a").text
