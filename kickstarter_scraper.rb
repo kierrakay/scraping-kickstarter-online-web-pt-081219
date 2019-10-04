63b9ddac7c270879466bca6908f6aabac79362e3
Kierra M <k.merriman32@hotmail.com>
2019-10-04 22:04:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -28,7 +28,10 @@ require "pry"
     kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      
+         :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
  #using the to_sym method. Remember that symbols make better hash keys than strings.
